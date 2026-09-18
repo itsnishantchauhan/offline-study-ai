a3fc81c8c2cf9c6c21d86afcbade23836e8d65c6
Nishant Chauhan 21 <chauhannishant697@gmail.in>
2026-09-18 12:43:43 +0530

    Connect frontend with FastAPI backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,18 +1,36 @@
-const questionInput= document.querySelector("#question");
-const askButton= document.querySelector("#askButton");
-const chat= document.querySelector("#chat");
+const questionInput = document.getElementById("question");
+const askButton = document.getElementById("askButton");
+const chat = document.getElementById("chat");
 
-askButton.addEventListener("click", () => {
+askButton.addEventListener("click", async function () {
 
     const question = questionInput.value;
-    
+
     if (question === "") {
         return;
     }
 
     const userMessage = document.createElement("p");
-
     userMessage.textContent = "You: " + question;
     chat.appendChild(userMessage);
+
     questionInput.value = "";
+
+
+    const response = await fetch("http://127.0.0.1:8000/ask", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            question: question
+        })
+    });
+
+    const result = await response.json();
+    
+    const aiMessage = document.createElement("p");
+
+    aiMessage.textContent = "AI: " + result.answer;
+    chat.appendChild(aiMessage);
 });
